7bd9d7c2dda7d5b81d7bef0a8231eed08ce8d1e8
Jarles Sayhare <jarlesjrlz@gmail.com>
2022-04-09 03:07:38 -0300

    adicionando arquivos de estudo

---
+++ Condições Simples/Compostas/TerminalDetran.js
@@ -0,0 +1,11 @@
+var vel = 80
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+
+    console.log(`Você ultrapassou a velocidade permitida.MULTADO!!!`)
+
+}
+
+console.log(`Diriga sempre utilizando o cinto de segurança!`)
